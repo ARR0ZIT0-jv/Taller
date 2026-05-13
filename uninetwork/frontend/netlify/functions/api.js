368873c5f44eadd2d6f70c96dac0cd01bf1ca542
alemanortegajorgeluis1 <alemanortegajorgeluis1@gmail.com>
2026-05-13 18:17:04 -0400

    hola

--- uninetwork/frontend/netlify/functions/api.js
+++ uninetwork/frontend/netlify/functions/api.js
@@ -1,12 +1,12 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/netlify';
-import { createClient } from '@libsql/client/web';
+import { createClient } from '@libsql/client';
 import { cors } from 'hono/cors';
 import { hashPassword, verifyPassword, generateToken, verifyToken } from './lib/auth.js';
 import { validateCI, normalizeCI } from './lib/ciValidator.js';
 import { calculateScores, generateProfileLabel, matchCareers, CATEGORIES, CATEGORY_LABELS, CATEGORY_DESCRIPTIONS } from './lib/riasecEngine.js';
 
-const app = new Hono();
+const app = new Hono().basePath('/.netlify/functions');
 
 app.use('*', cors({ origin: '*', allowMethods: ['GET','POST','PUT','DELETE','OPTIONS'], allowHeaders: ['Content-Type','Authorization'] }));
 
